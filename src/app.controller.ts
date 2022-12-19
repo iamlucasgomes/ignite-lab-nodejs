c8c11ae61298bfd31032030f6a69dcc8bc0ca783
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-18 21:58:48 -0300

    feat: validação adicionada

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
 @Controller('notifications')
 export class AppController {
@@ -12,13 +13,14 @@ export class AppController {
   }
 
   @Post()
-  async create() {
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
     await this.prismaService.notification.create({
       data: {
         id: randomUUID(),
-        content: 'Você tem uma nova notificação',
-        category: 'social',
-        recipientId: randomUUID(),
+        content,
+        category,
+        recipientId,
       },
     });
   }
