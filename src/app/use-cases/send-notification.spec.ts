dd9100a28232289e028df633e706a1aa4719c868
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-19 13:59:13 -0300

    feat: repositorio de notificações criado

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,14 +1,24 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Test send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'This is a notification',
       category: 'Social',
       recipientId: 'example-recipient-id',
     });
-    expect(notification).toBeTruthy();
+
+    expect(notifications).toHaveLength(1);
   });
 });
