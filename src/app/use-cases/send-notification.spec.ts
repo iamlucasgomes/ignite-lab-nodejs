a5606f60d56b5e726f62b175219dfcdc0155b2b6
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-19 14:13:15 -0300

    feat: aprendi o conceito do banco de dados em memoria

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,24 +1,18 @@
-import { Notification } from '../entities/notification';
+import { inMemoryNotificationRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Test send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification(notificationsRepository);
+    const notificationRepository = new inMemoryNotificationRepository();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: 'This is a notification',
       category: 'Social',
       recipientId: 'example-recipient-id',
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationRepository.notifications).toHaveLength(1);
+    expect(notificationRepository.notifications.at(0)).toEqual(notification);
   });
 });
