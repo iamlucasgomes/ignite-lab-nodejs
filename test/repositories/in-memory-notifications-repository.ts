3dfd8a3740c874b24156e789e7b505638516178a
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-20 23:41:00 -0300

    feat: listagem por recipiente completa

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -12,6 +12,13 @@ export class inMemoryNotificationRepository implements NotificationRepository {
     }
     return notification;
   }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
+  }
+
   async countManyByRecipientId(recipientId: string): Promise<number> {
     return this.notifications.filter(
       (notification) => notification.recipientId === recipientId,
